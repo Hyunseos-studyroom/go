63671c72dbbc12445f6a83d3f93b86d68c999ff6
3boku <hyunseojung@clika.io>
2024-10-11 17:56:41 +0900

    asdfsdaffsad

--- kakao/main.go
+++ kakao/main.go
@@ -8,18 +8,28 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.LoadHTMLGlob("layout/*")
-
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "메인페이지",
-		})
-	})
 	r.POST("/login", Login)
 
 	r.Run(":8080")
 }
 
 func Login(c *gin.Context) {
+	var requestBody struct {
+		Code string `json:"code"`
+	}
 
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON provided"})
+		return
+	}
+
+	// 여기에 실제 토큰 발급 로직 추가
+	accessToken := "your_access_token"
+	refreshToken := "your_refresh_token"
+
+	// 쿠키 설정
+	c.SetCookie("access_token", accessToken, 3600, "/", "localhost", false, true)         // Secure: true로 설정하면 HTTPS에서만 쿠키 전송
+	c.SetCookie("refresh_token", refreshToken, 3600*24*30, "/", "localhost", false, true) // 30일 유효
+
+	c.JSON(http.StatusOK, gin.H{"message": "Logged in successfully"})
 }
---
+++ kakao/react/src/home.tsx
@@ -0,0 +1,11 @@
+// Home.tsx
+const Home = () => {
+    return (
+        <div>
+            <h1>환영합니다!</h1>
+            <p>로그인이 성공적으로 완료되었습니다.</p>
+        </div>
+    );
+};
+
+export default Home;
