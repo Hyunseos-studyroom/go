67365d9d7c471c311b37ee3709bab0013c830150
3boku <hyunseojung@clika.io>
2024-10-29 12:02:04 +0900

    fyne

---
+++ fyne/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello") // 윈도우 명칭 지정
+
+	hello := widget.NewLabel("Hello World!")
+	w.SetContent(
+		container.NewVBox( // 컨텐츠 수직 정렬
+			hello,
+			widget.NewButton(
+				"Hi!",                                  // 버튼 명칭
+				func() { hello.SetText("Welcome :)") }, // 버튼 이벤트
+			),
+		),
+	)
+
+	w.ShowAndRun() // 윈도우 출력
+}
