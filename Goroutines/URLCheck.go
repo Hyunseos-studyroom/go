f5c5f336e41c836b0fc8bfb29ffa1dc91d0c45e6
3boku <hyunseojung@clika.io>
2024-10-29 12:02:21 +0900

    a

--- Goroutines/URLCheck.go
+++ Goroutines/URLCheck.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	var results = make(map[string]string)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		result := "OK"
+		err := higURL(url)
+		if err != nil {
+			result = "ERROR"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func higURL(url string) (err error) {
+	fmt.Println("CHecking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return err
+	}
+	return nil
+}
--- Goroutines/channel.go
+++ Goroutines/channel.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan string)
+	people := [2]string{"John", "Jessi"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+}
+
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	fmt.Println(person)
+	c <- person + "is sexy"
+}
